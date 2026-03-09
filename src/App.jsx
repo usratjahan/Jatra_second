0ce12d2ad1930a587ee7ad122142750c1d9f281b
Usrat Jahan <usratjahan642@gmail.com>
2026-03-09 13:57:42 +0600

    i have pushed the stucture of react

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import Home from './pages/Home'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+
+      {/* define routes so <Link> and useLocation work */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/events" element={<div className="mt-20 p-4">Events (coming soon)</div>} />
+        <Route path="/explore" element={<div className="mt-20 p-4">Explore (coming soon)</div>} />
+        <Route path="/contact" element={<div className="mt-20 p-4">Contact (coming soon)</div>} />
+        {/* add additional routes as needed */}
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  )
+}
+
+export default App
